f71110ad062755a3ab4c96ad06f7b11f3a8050af
bunny-polygon-dev <86653165+bunny-polygon-dev@users.noreply.github.com>
2021-08-11 10:55:51 +0900

    Update IZap.sol

--- contracts/interfaces/IZap.sol
+++ contracts/interfaces/IZap.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.6.12;
 
 interface IZap {
+    function covers(address _token) external view returns (bool);
     function zapOut(address _from, uint amount) external;
     function zapIn(address _to) external payable;
     function zapInToken(address _from, uint amount, address _to) external;
